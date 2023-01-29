83b6a6d597a89fafb5ff2c3cc4623ed12a3b7efc
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-01-29 00:41:28 -0300

    refactor: updated project files

--- src/infra/database/dbConfigStatic.js
+++ src/infra/database/dbConfigStatic.js
@@ -4,7 +4,7 @@ module.exports = {
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
-	charset: "utf8",
+	charset: 'utf8',
 	dialect: process.env.DB_DBMS_NAME,
 	port: process.env.DB_PORT,
 	define: {

7dda3f9cbf01b031df50e4c251d98e694b2d6b42
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-03-08 14:29:07 -0300

    docs: updated swagger path

--- src/interface/http/middlewares/swaggerMiddleware.ts
+++ src/interface/http/middlewares/swaggerMiddleware.ts
@@ -2,7 +2,7 @@ import YAML from 'yamljs';
 import swaggerUi from 'swagger-ui-express';
 
 
-const swaggerDocument = YAML.load('src/interface/http/docs/swagger.yaml');
+const swaggerDocument = YAML.load('docs/swagger.yaml');
 
 export default () => [
 	swaggerUi.serve, swaggerUi.setup(swaggerDocument),

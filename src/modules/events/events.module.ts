b9af7349df05c8473e2182d2aeed897385dfb95a
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-07-02 15:00:11 -0300

    feat: queue consumer

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -10,7 +10,17 @@ import EventsQueueProducer from '@modules/events/queue/producers/EventsQueueProd
 @Module({
 	imports: [
 		SqsModule.register({
-			consumers: [],
+			consumers: [
+				{
+					name: process.env.AWS_SQS_EVENTS_QUEUE_NAME || 'eventsQueue.fifo',
+					queueUrl: process.env.AWS_SQS_EVENTS_QUEUE_URL || 'http://localhost:4566/000000000000/eventsQueue.fifo',
+					region: process.env.AWS_REGION || 'us-east-1',
+					batchSize: 10,
+					shouldDeleteMessages: true,
+					handleMessageTimeout: 1000,
+					waitTimeSeconds: 20,
+				},
+			],
 			producers: [],
 		}),
 	],
--- src/modules/events/queue/consumers/EventsQueueConsumer.ts
+++ src/modules/events/queue/consumers/EventsQueueConsumer.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { SqsMessageHandler, SqsConsumerEventHandler } from '@ssut/nestjs-sqs';
 import { Message } from '@aws-sdk/client-sqs';
 import { Logger } from 'winston';
@@ -7,30 +6,27 @@ import SubscriptionService from 'src/modules/app/services/SubscriptionService';
 import LoggerGenerator from '@infra/logging/LoggerGenerator';
 import Exceptions from '@infra/errors/Exceptions';
 import eventSchema from '../schemas/eventSchema';
-import { ConfigsInterface } from '@configs/configs';
 import dotenv from 'dotenv';
 dotenv.config();
 
 
-const queueUrl = process.env.AWS_SQS_EVENTS_QUEUE_URL || 'http://localhost:4566/000000000000/eventsQueue.fifo';
+const eventsQueueName = process.env.AWS_SQS_EVENTS_QUEUE_NAME || 'eventsQueue.fifo';
 @Injectable()
 export default class EventsQueueConsumer {
 	private readonly logger: Logger;
 	private readonly sqsQueueName: string;
 
 	constructor(
-		private readonly configService: ConfigService,
 		private readonly subscriptionService: SubscriptionService,
 		private readonly loggerGenerator: LoggerGenerator,
 		private readonly exceptions: Exceptions,
 	) {
 		this.logger = this.loggerGenerator.getLogger();
-		const { queueName }: ConfigsInterface['integration']['aws']['sqs']['eventsQueue'] = this.configService.get<any>('integration.aws.sqs.eventsQueue');
-		this.sqsQueueName = queueName || 'eventsQueue.fifo';
+		this.sqsQueueName = EventsQueueConsumer.name;
 	}
 
-	@SqsMessageHandler(queueUrl, true)
-	public async handleMessageBatch(messages: Message[]) {
+	@SqsMessageHandler(eventsQueueName, true)
+	public async handleMessageBatch(messages: Message[]): Promise<void> {
 		for (const message of messages) {
 			try {
 				this.logger.info(`New message received from [${this.sqsQueueName}]`);
@@ -57,13 +53,13 @@ export default class EventsQueueConsumer {
 		}
 	}
 
-	@SqsConsumerEventHandler(queueUrl, 'error')
-	public onError(error: Error, message: Message) {
-		this.logger.error(`[${EventsQueueConsumer.name}] event error. QueueName: ${this.sqsQueueName} MessageId: ${message.MessageId} Error: ${error.message}`);
+	@SqsConsumerEventHandler(eventsQueueName, 'error')
+	public onError(error: Error, message: Message): void {
+		this.logger.error(`[${EventsQueueConsumer.name}] event error from [${this.sqsQueueName}] - MessageId: ${message.MessageId}. Error: ${error.message}`);
 	}
 
-	@SqsConsumerEventHandler(queueUrl, 'processing_error')
-	public onProcessingError(error: Error, message: Message) {
-		this.logger.error(`[${EventsQueueConsumer.name}] processing error. QueueName: ${this.sqsQueueName} MessageId: ${message.MessageId} Error: ${error.message}`);
+	@SqsConsumerEventHandler(eventsQueueName, 'processing_error')
+	public onProcessingError(error: Error, message: Message): void {
+		this.logger.error(`[${EventsQueueConsumer.name}] processing error from [${this.sqsQueueName}] - MessageId: ${message.MessageId}. Error: ${error.message}`);
 	}
 }

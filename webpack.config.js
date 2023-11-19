861ff378af527ccfe8480dc3283d5cf2e9928550
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-11-18 22:49:24 -0300

    build: updated webpack configfile

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	entry: ['webpack/hot/poll?1000', './build/src/main.js'],
-	watch: true,
+	watch: false,
 	target: 'node',
 	externals: [
 		nodeExternals({

c96b190ddac41c68972b1c9bc1400141990c8a46
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2024-01-07 04:00:52 -0300

    feat(security & perf): added throttler

--- src/modules/api/api.module.ts
+++ src/modules/api/api.module.ts
@@ -1,6 +1,9 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import HttpConstants from '@api/constants/Http.constants';
 import ContentTypeConstants from './constants/ContentType.constants';
+import RequestRateConstants from './constants/RequestRate.constants';
 import LoggerMiddleware from '@api/middlewares/Logger.middleware';
 import JwtDecodeMiddleware from '@api/middlewares/JwtDecode.middleware';
 import DefaultController from '@api/controllers/Default.controller';
@@ -8,16 +11,29 @@ import UserController from '@api/controllers/User.controller';
 import SubscriptionsController from '@api/controllers/Subscriptions.controller';
 
 
+const requestRateConstants = new RequestRateConstants();
+
 @Module({
-	imports: [],
+	imports: [
+		ThrottlerModule.forRoot([
+			requestRateConstants.short,
+			requestRateConstants.medium,
+			requestRateConstants.long,
+		]),
+	],
 	controllers: [
 		DefaultController,
 		UserController,
 		SubscriptionsController,
 	],
 	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard,
+		},
 		HttpConstants,
 		ContentTypeConstants,
+		RequestRateConstants,
 	],
 	exports: [],
 })

6aba13d1916e0db70b9db68103493304887a94fd
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2024-02-18 13:39:01 -0300

    build: updated packages and removed vulnerabilities

--- src/modules/common/utils/helpers/DateGenerator.helper.ts
+++ src/modules/common/utils/helpers/DateGenerator.helper.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export default class DateGeneratorHelper {
 	public getDate(withoutTimezone = true, dateStr?: string): Date {
-		const date = !!dateStr ? new Date(dateStr) : new Date();
+		const date = dateStr ? new Date(dateStr) : new Date();
 		const strDate = date.toISOString();
 
 		return new Date(withoutTimezone ? strDate.slice(0, -1) : strDate);

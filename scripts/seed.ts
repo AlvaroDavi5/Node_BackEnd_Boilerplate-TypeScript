8e5ce8a0b99e08e04fcce9ad2d605fa347f6bd3c
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2024-09-12 18:14:16 -0300

    chore: added seed script

---
+++ scripts/seed.ts
@@ -0,0 +1,50 @@
+import { DataSource } from 'typeorm';
+import { dbConfig } from '@core/infra/database/db.config';
+import { testConnection } from '@core/infra/database/connection';
+import UsersModel from '@core/infra/database/models/Users.model';
+import UserPreferencesModel from '@core/infra/database/models/UserPreferences.model';
+
+
+async function getConnection() {
+	const connection = new DataSource(dbConfig);
+
+	const isInitialized = await testConnection(connection, console);
+
+	if (isInitialized)
+		return connection;
+
+	return await connection.initialize();
+}
+
+function getRepositories(connection: DataSource) {
+	UsersModel.useDataSource(connection);
+	UserPreferencesModel.useDataSource(connection);
+
+	const userRepository = UsersModel.getRepository();
+	const userPreferenceRepository = UserPreferencesModel.getRepository();
+
+	return {
+		userRepository,
+		userPreferenceRepository,
+	};
+}
+
+async function seedDatabase() {
+	const connection = await getConnection();
+	const { userRepository, userPreferenceRepository } = getRepositories(connection);
+
+	console.info(
+		'\n # Seeding database \n'
+	);
+
+	const testUser = await userRepository.create({
+		email: 'tester.user@nomail.com',
+		password: '$2b$10$KuaNFAgwY.RJfRLhS0kDVu|D73SxsNl_H6yaDzjE_-ZAJCorFvEnN1WG3bn8ICGA-Y',
+	}).save();
+	await userPreferenceRepository.create({
+		user: { id: testUser.id },
+		defaultTheme: 'INVALID',
+	}).save();
+}
+
+seedDatabase();

53ffba630aa215250402bee4925d41202d90b819
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2025-03-11 21:51:12 -0300

    feat: added custom throttler decorator

--- src/modules/common/guards/CustomThrottler.guard.ts
+++ src/modules/common/guards/CustomThrottler.guard.ts
@@ -1,23 +1,26 @@
+import { Reflector } from '@nestjs/core';
 import { Injectable, Scope, ExecutionContext, CanActivate } from '@nestjs/common';
 import { InjectThrottlerStorage, ThrottlerStorage, InjectThrottlerOptions, ThrottlerModuleOptions, ThrottlerOptions, Resolvable } from '@nestjs/throttler';
 import { HttpArgumentsHost, WsArgumentsHost } from '@nestjs/common/interfaces';
 import { Socket } from 'socket.io-client';
 import Exceptions from '@core/errors/Exceptions';
 import LoggerService from '@core/logging/Logger.service';
+import { CUSTOM_THROTTLER_DECORATOR } from '@api/decorators/customThrottler.decorator';
 import { RequestInterface, ResponseInterface } from '@shared/internal/interfaces/endpointInterface';
 
 @Injectable({ scope: Scope.DEFAULT })
 export default class CustomThrottlerGuard implements CanActivate {
-	private readonly throttlers: ThrottlerOptions[];
+	private readonly globalThrottlers: ThrottlerOptions[];
 	private readonly headerPrefix: string;
 
 	constructor(
+		private readonly reflector: Reflector,
 		@InjectThrottlerOptions() private readonly throttlerModuleOptions: ThrottlerModuleOptions,
 		@InjectThrottlerStorage() private readonly throttlerStorage: ThrottlerStorage,
 		private readonly exceptions: Exceptions,
 		private readonly logger: LoggerService,
 	) {
-		this.throttlers = Array.isArray(this.throttlerModuleOptions)
+		this.globalThrottlers = Array.isArray(this.throttlerModuleOptions)
 			? this.throttlerModuleOptions
 			: this.throttlerModuleOptions.throttlers;
 		this.headerPrefix = 'X-RateLimit';
@@ -27,7 +30,7 @@ export default class CustomThrottlerGuard implements CanActivate {
 		const contextType = context.getType();
 
 		const continues: boolean[] = [];
-		for (const throttler of this.throttlers) {
+		for (const throttler of [...this.globalThrottlers, ...this.getCustomThrottlers(context)]) {
 			const { name: throttlerName, limit, ttl } = await this.getThrottlerData(context, throttler);
 			const { skipIf } = throttler;
 
@@ -95,6 +98,18 @@ export default class CustomThrottlerGuard implements CanActivate {
 
 	private generateKey(throttlerName: string, clientContext: string, clientId: string, callCode: string): string {
 		return `${CustomThrottlerGuard.name}:${throttlerName}:${clientContext}:${clientId}:${callCode}`;
+	}
+
+	private getCustomThrottlers(context: ExecutionContext): ThrottlerOptions[] {
+		const throttlers: ThrottlerOptions[] = [];
+
+		const methodCustomThrottler = this.reflector.get<ThrottlerOptions, string>(CUSTOM_THROTTLER_DECORATOR, context.getHandler());
+		const classCustomThrottler = this.reflector.get<ThrottlerOptions, string>(CUSTOM_THROTTLER_DECORATOR, context.getClass());
+
+		if (!!methodCustomThrottler) throttlers.push(methodCustomThrottler);
+		if (!!classCustomThrottler) throttlers.push(classCustomThrottler);
+
+		return throttlers;
 	}
 
 	private async getThrottlerData(context: ExecutionContext, throttler: ThrottlerOptions): Promise<{ name: string, limit: number, ttl: number }> {
